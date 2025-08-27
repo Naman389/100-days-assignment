7ca69d95852dc986672035e19e6a9b2e7ea1488a
Naman389 <namansingh6386@gmail.com>
2025-08-27 15:57:36 +0530

    Add files via upload
    DAY 7 Assignment

---
+++ Q14.c
@@ -0,0 +1,34 @@
+//Q14: Write a program to input a character and check whether it is a vowel or consonant using if–else.
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter alphabet:");
+    scanf("%c",&ch);
+    
+ if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
+            ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            printf("%c is a vowel.\n", ch);
+            }else{
+                printf("%c is a consonant",ch);
+            }
+            }
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
