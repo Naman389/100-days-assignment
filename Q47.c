be7ff0337483a13d2b95a1f002a6a1aa9c9046f1
Naman389 <namansingh6386@gmail.com>
2025-09-13 12:54:55 +0530

    Add files via upload
    DAY 24 Assignment

---
+++ Q47.c
@@ -0,0 +1,32 @@
+//Q47: Write a program to print the following pattern:
+// *
+// **
+// ***
+// ****
+// *****
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+**
+***
+****
+*****
+
+*/
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    printf("Enter rows :");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+            for(j=0;j<=i;j++){
+             printf("*");
+         }
+         printf("\n");
+    }
+    return 0;
+}
