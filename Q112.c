01b97798e5a987f31f8ffe41b936127f3ee00b3d
Naman389 <namansingh6386@gmail.com>
2025-11-19 15:31:21 +0530

    Add files via upload

--- Q112.c
+++ Q112.c
@@ -30,20 +30,21 @@ int main() {
     for(int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
-    int max_sum = arr[0];
-    int current_sum = arr[0];
+    int maxsum = arr[0];
+    int currentsum = arr[0];
 
     for(int i = 1; i < n; i++) {
-        if(current_sum + arr[i] > arr[i])
-            current_sum += arr[i];
+        if(currentsum + arr[i] > arr[i])
+            currentsum += arr[i];
         else
-            current_sum = arr[i];
+            currentsum = arr[i];
 
-        if(current_sum > max_sum)
-            max_sum = current_sum;
+        if(currentsum > maxsum)
+            maxsum = currentsum;
     }
 
-    printf("Maximum subarray sum = %d\n", max_sum);
+    printf("Maximum subarray sum = %d\n", maxsum);
 
     return 0;
 }
+
